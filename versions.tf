05f1cc6de4617ef7fdb2a1401468ca3161f94b8d
Alberto Tablado <alberto@aluki.org>
2025-07-24 00:54:09 +0200

    fix: provider termplate

--- versions.tf
+++ versions.tf
@@ -1,7 +1,9 @@
 terraform {
   required_version = "~> 1.8"
-  required_providers {
-#    provider "a" {
+#  required_providers {
+#    provider = {
+#      source = "source"
+#      version = "~> version"
 #    }
-  }
+#  }
 }
